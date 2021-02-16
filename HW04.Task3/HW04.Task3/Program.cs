ca109f1176b3350cb1df50d5ab39ffe45d9f023f
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-02-16 22:22:36 +0300

    added task 3 HW04

---
+++ HW04.Task3/HW04.Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HW04.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, number3, number4;
+
+            Console.WriteLine("Enter number 1");
+            string num1Txt = Console.ReadLine();
+            number1 = Convert.ToInt32(num1Txt);
+
+            Console.WriteLine("Enter number 2");
+            string num2Txt = Console.ReadLine();
+            number2 = Convert.ToInt32(num2Txt);
+
+            Console.WriteLine("Enter the result:", number1 - number2);
+            string num3Txt = Console.ReadLine();
+            number3 = int.Parse(num3Txt);
+
+            number4 = number1 - number2;
+
+            if (number4 == number3)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect!");
+                if (number3 < number4)
+                {
+                    Console.WriteLine("The number should be higher");
+                }
+                if (number3 > number4)
+                { 
+                    Console.WriteLine("The number should be smaller");
+                }
+            }   
+        }
+    }
+}
