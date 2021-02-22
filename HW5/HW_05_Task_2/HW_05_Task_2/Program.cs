35a36453d312c3d4f9e4dc0cf7a593c1d9184c86
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-02-22 16:16:10 +0300

    added HW3 task with calculator version 3

---
+++ HW5/HW_05_Task_2/HW_05_Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW_05_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number!");
+            string yourword = Console.ReadLine();
+        }
+    }
+}
