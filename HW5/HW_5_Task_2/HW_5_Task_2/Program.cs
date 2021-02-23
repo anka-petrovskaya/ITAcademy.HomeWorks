d71bc7c617089d892fad6ab51ceedced604c2e7d
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-02-23 15:28:30 +0300

    added task 2

---
+++ HW5/HW_5_Task_2/HW_5_Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW_5_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number");
+
+        }
+    }
+}
