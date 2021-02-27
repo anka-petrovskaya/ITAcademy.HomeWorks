9703c3719139fdb9a74f304ac1b835e8d6b75928
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-02-27 16:15:03 +0300

    added task 2

--- HW7/HW_7_Task_2/HW_7_Task_2/Program.cs
+++ HW7/HW_7_Task_2/HW_7_Task_2/Program.cs
@@ -17,9 +17,7 @@ namespace HW_7_Task_2
             foreach (string word in words)
                 if (word.Length > max)
                     max = word.Length;
-            Console.WriteLine(max);
             strTxt = strTxt.Remove(max);
-            Console.WriteLine(strTxt);
         }
         static void ChangePlaces(string strTxt)
         {
@@ -30,14 +28,11 @@ namespace HW_7_Task_2
             foreach (string word in words)
                 if (word.Length > max)
                     max = word.Length;
-            Console.WriteLine(max);
 
             foreach (string word in words)
                 if (word.Length < min)
                     min = word.Length;
-            Console.WriteLine(min);
             strTxt = strTxt.Replace((char)max, (char)min);
-            Console.WriteLine(strTxt);
         }
         static void CountSymbols(string strTxt)
         {
@@ -47,22 +42,18 @@ namespace HW_7_Task_2
         static void MyArray(string strTxt)
         {
             string[] words = strTxt.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-            int max = 0;
-            int min = 1;
-
-            foreach (string word in words)
-                if (word.Length < min)
-                    min = word.Length;
-            foreach (string word in words)
-                if (word.Length > max)
-                    max = word.Length;
-
-
-            for (int i = 0; i < length; i++)
-            {
-
-            }
+            char[] myArray = strTxt.ToCharArray();
+            char symb;
+            for (int i = 0; i < myArray.Length; i++)
+                for (int j = 0; j < myArray.Length; j++)
+                {
+                    if (myArray[j] > myArray[j+1])
+                    {
+                        symb = myArray[j + 1];
+                        myArray[j + 1] = myArray[j];
+                        myArray[j] = symb;
+                    }
+                }
         }
     }
 }
