dbcfe18ce3f2fd2a83aedfa475dd65554e9e3b3d
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-03-03 12:11:19 +0300

    added task 1

---
+++ HW8/HW_08_Tak_1/HW_08_Tak_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace HW_08_Tak_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder str = new StringBuilder("1a!2.3!!..4.!.? 6 7!..");
+            Console.WriteLine(str);
+
+            str.Replace("\\", "");
+            Console.WriteLine(str);
+
+            str.Replace(" ", "_");
+            Console.WriteLine(str);
+        }
+    }
+}
