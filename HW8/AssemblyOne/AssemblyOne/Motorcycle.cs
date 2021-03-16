e25f016003197bb1f17e4956f092c1de23b10f00
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-03-16 22:45:21 +0300

    HW_08_task_3

---
+++ HW8/AssemblyOne/AssemblyOne/Motorcycle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssemblyOne
+{
+    public class Motorcycle
+    {
+        public const ushort MaxSpeed = 300;
+        public string vinNumber = "2sd34fdsd";
+        public int odmeter = 20;
+        public void StartEngine()
+        {
+        }
+
+        internal const ushort MaxSpeedInternal = 301;
+        internal string vinNumberInt = "2sd34fdsd";
+        internal int odmeterInt = 20;
+        internal void StartDate()
+        {
+        }
+
+        protected const ushort MaxSpeedProtected = 301;
+        protected string vinNumberPro = "2sd34fdsd";
+        protected int odmeterPro = 20;
+        protected void StartSpeed()
+        {
+        }
+
+        private const ushort MaxSpeedPrivate = 301;
+        private string vinNumberPrivate = "2sd34fdsd";
+        private int odmeterPrivate = 20;
+        private void odmeterPri()
+        {
+        }
+    }
+}
---
+++ HW8/HW_08_Tak_2/HW_08_Tak_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using ImTools;
+using System;
+
+namespace HW_08_Tak_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am" +18 + ".");
+            Console.WriteLine("I am" +18.ToString() + ".");
+            Console.WriteLine($"I am { 18}.");
+        }
+    }
+}
